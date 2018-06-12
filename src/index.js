b0692641792eef567ba96abe8718bfb0629244eb
Marcin Ryzycki <marcin@ryzycki.com>
2018-06-12 18:33:37 +0100

    feat: example with jQuery

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import { doSomething } from './module-1';
+import { doSomething, testJQuery } from './module-1';
 import './styles/app.scss';
 
 doSomething();
+testJQuery();
--- src/module-1.js
+++ src/module-1.js
@@ -1,5 +1,10 @@
-import './styles/module-1.scss';
+import $ from 'jquery';
 
 export function doSomething() {
-  console.log('doSomethingggg:', 2**32);
+  console.log('doSomething:', 2**32);
+}
+
+export function testJQuery() {
+  console.log('jQuery loaded, click on the headline');
+  $('h1').after(`<p>Hey, that's added by jQuery.</p>`);
 }
